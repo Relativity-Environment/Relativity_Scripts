e78689c0751a8e70fbaa66462325f9c978107936
VictorGil <victorgilasp@gmail.com>
2020-07-05 23:09:56 +0200

    commit

--- minimal/install.minimal.ps1
+++ minimal/install.minimal.ps1
@@ -79,6 +79,71 @@ $ManualDownloadInstall = @{
     'Nessus-8.10.1-x64.msi' = 'http://52.210.171.72/gravity/Nessus-8.10.1-x64.msi'
 }
 
+function Get-DownloadManual($UtilDownloadPath, $UtilBinPath)
+{
+
+    [Net.ServicePointManager]::SecurityProtocol=[System.Security.Authentication.SslProtocols] "tls, tls11, tls12"
+    
+    $FilesDownloaded = @()
+
+    If (-not (Test-Path $UtilDownloadPath)) {
+        mkdir $UtilDownloadPath -Force
+    }
+
+    If (-not (Test-Path $UtilBinPath)) {
+        mkdir $UtilBinPath -Force
+    }
+    
+    Push-Location $UtilDownloadPath
+    
+    Foreach($software in $ManualDownloadInstall.keys) {
+    
+    Write-Output "Downloading $software"
+    if ( -not (Test-Path $software) ) {
+        try {
+                Invoke-WebRequest $ManualDownloadInstall[$software] -OutFile $software -UseBasicParsing
+                $FilesDownloaded += $software
+        }
+        catch {}
+    }
+    else {
+
+            Write-Warning "File is already downloaded, skipping: $software"
+        }
+    }
+
+}
+
+function Install-Zip($UtilDownloadPath, $UtilBinPath)
+{
+
+    # zip installs
+    Write-Output 'Extracting self-contained binaries (zip files) to our bin folder'
+    Get-ChildItem -Path $UtilDownloadPath -File -Filter '*.zip' | Where-Object {$FilesDownloaded -contains $_.Name} | ForEach-Object {
+    Expand-Archive -Path $_.FullName -DestinationPath $UtilBinPath -Force
+    Add-EnvPath -Location 'machine' -NewPath $UtilBinPath
+    }
+
+}
+
+function Install-Soft($UtilDownloadPath)
+{
+
+         
+     # exe installs
+     Get-ChildItem -Path $UtilDownloadPath -File -Filter '*.exe' | Where-Object {$FilesDownloaded -contains $_.Name} | ForEach-Object {
+     Start-Proc -Exe $_.FullName -waitforexit
+     }
+ 
+     # msi installs
+     Get-ChildItem -Path $UtilDownloadPath -File -Filter '*.msi' | Where-Object {$FilesDownloaded -contains $_.Name} | ForEach-Object {
+     Start-Proc -Exe $_.FullName -waitforexit
+     }
+
+
+
+}
+
 
 $UtilBinPath = "$env:SystemDrive\Analisis de Vulnerabilidades"
 Get-DownloadManual -UtilDownloadPath "C:\tmp\vuls" -UtilBinPath "$UtilBinPath"
