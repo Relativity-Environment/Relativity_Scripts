42b29faaadbb77023100d9f4a5d45eca50bf6f54
VictorGil <victorgilasp@gmail.com>
2020-07-05 23:45:30 +0200

    commit

--- module/module.psm1
+++ module/module.psm1
@@ -50,7 +50,7 @@ function Add-Folders{
 }
 
 
-function Get-DownloadManual($UtilDownloadPath)
+function Get-DownloadManual
 {
 
     [Net.ServicePointManager]::SecurityProtocol=[System.Security.Authentication.SslProtocols] "tls, tls11, tls12"
