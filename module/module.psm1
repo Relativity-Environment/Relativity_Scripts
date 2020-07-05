2f5c021c9e2b931487bf8001145d9c472afbb043
VictorGil <victorgilasp@gmail.com>
2020-07-05 23:00:56 +0200

    commit

--- module/module.psm1
+++ module/module.psm1
@@ -85,18 +85,22 @@ function Get-DownloadManual($UtilDownloadPath, $UtilBinPath)
 
 }
 
-
-function Install-Soft($UtilDownloadPath, $UtilBinPath)
+functio Install-Zip($UtilDownloadPath, $UtilBinPath)
 {
 
-     # zip installs
-     Write-Output 'Extracting self-contained binaries (zip files) to our bin folder'
-     Get-ChildItem -Path $UtilDownloadPath -File -Filter '*.zip' | Where-Object {$FilesDownloaded -contains $_.Name} | ForEach-Object {
-     Expand-Archive -Path $_.FullName -DestinationPath $UtilBinPath -Force
-     }
- 
-     Add-EnvPath -Location 'machine' -NewPath $UtilBinPath
- 
+    # zip installs
+    Write-Output 'Extracting self-contained binaries (zip files) to our bin folder'
+    Get-ChildItem -Path $UtilDownloadPath -File -Filter '*.zip' | Where-Object {$FilesDownloaded -contains $_.Name} | ForEach-Object {
+    Expand-Archive -Path $_.FullName -DestinationPath $UtilBinPath -Force
+    Add-EnvPath -Location 'machine' -NewPath $UtilBinPath
+    }
+
+}
+
+function Install-Soft($UtilDownloadPath)
+{
+
+         
      # exe installs
      Get-ChildItem -Path $UtilDownloadPath -File -Filter '*.exe' | Where-Object {$FilesDownloaded -contains $_.Name} | ForEach-Object {
      Start-Proc -Exe $_.FullName -waitforexit
