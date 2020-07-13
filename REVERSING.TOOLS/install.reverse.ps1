7f23048a81130af8631513f65e66508a90dbf5e5
VictorGil <victorgilasp@gmail.com>
2020-07-13 21:57:13 +0200

    commit

--- REVERSING.TOOLS/install.reverse.ps1
+++ REVERSING.TOOLS/install.reverse.ps1
@@ -3,7 +3,7 @@
 
 $BoxPackageName   =   "install.full"
 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1 -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "SecurityHealth" -ErrorAction SilentlyContinue
 
 if (Test-PendingReboot) { Invoke-Reboot }   
