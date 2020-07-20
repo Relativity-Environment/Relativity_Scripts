61029b9037a567c748e51b244488df12ee3c9d25
VictorGil <victorgilasp@gmail.com>
2020-07-21 00:34:09 +0200

    commit

--- MODULE/module.psm1
+++ MODULE/module.psm1
@@ -1,4 +1,12 @@
-﻿
+﻿function Add-PSContextualMenu{
+
+    pwsh.exe -command {Set-ExecutionPolicy Bypass -Scope Process -Force; `
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;`
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/lextm/windowsterminal-shell/master/install.ps1'))}
+
+}
+
+
 ## DISKSPACE & CHKPOINT
 Function Test-DiskSpace([string]$instalacion)
 {
